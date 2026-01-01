1e1e80fa964c7972e69bcdab15d52d314a372429
Mano Vikram <manovik18@gmail.com>
2026-01-02 00:21:59 +0530

    Added the database config details

---
+++ services/database/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+db_host = os.getenv("DB_HOST")
+db_port = int(os.getenv("DB_PORT"))
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+DB_CONFIG = {
+    "host": db_host,
+    "port": db_port,
+    "database": db_name,
+    "user": db_user,
+    "password": db_password,
+}

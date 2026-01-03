ce44a907879026e0aadbc4439553f6f6460a5360
Mano Vikram <manovik18@gmail.com>
2026-01-04 00:03:48 +0530

    Added route / endpoint to send the user query and to return the results from the DB

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
+	"github.com/ManoVikram/NL-To-SQL-With-AI/api/handlers"
 	"github.com/ManoVikram/NL-To-SQL-With-AI/api/services"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine, services *services.Service) {
-
+	server.POST("api/query", handlers.QueryHandler(services))
 }
